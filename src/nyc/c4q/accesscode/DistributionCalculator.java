93f9497fff9bb8187c761cbb7254c0bb4b5b835f
MadelynTav <Madelyntav@gmail.com>
2015-04-13 03:23:56 -0400

    updating hw

--- src/nyc/c4q/accesscode/DistributionCalculator.java
+++ src/nyc/c4q/accesscode/DistributionCalculator.java
@@ -1,8 +1,11 @@
 package nyc.c4q.accesscode;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.text.DecimalFormat;
+
 
 /**
  * Created by c4q-madelyntavarez on 4/1/15.
@@ -23,38 +26,76 @@ import java.util.Scanner;
 public class DistributionCalculator
 {
 
-    public static ArrayList calculate(File myfile)
-    {
+    public static ArrayList<String> calculate (File myFile) {
 
-        myfile = new File("/Users/c4q-madelyntavarez/Desktop/ScriptHW.txt");
+        ArrayList<String> percent = new ArrayList();
 
-        try
-        {
-            Scanner scanner = new Scanner(myfile);
-            while(scanner.hasNext())
-            {
-                System.out.println(scanner.next());
+        String beforeFile;
+        String afterFile = "";
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+
+        try {
+            Scanner input = new Scanner(myFile);
+
+            while (input.hasNext()) {
+
+                beforeFile = input.next();
+                beforeFile = beforeFile.toLowerCase();
+
+                for (int i=0; i < beforeFile.length(); i++) {
+                    String letter = beforeFile.substring(i, i+1);
+                    if (letter.matches("[a-z]"))
+                        beforeFile += letter;
+                    else
+                        continue;
+                }
             }
-        }
-        catch(Exception e)
-        {
 
-        }
-
-        ArrayList<String> myArray = new ArrayList<String>();
-        for (String letter
-
-
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
+        for (int i = 0; i < letters.length(); i++) {
+            percent.add(distribution(letters.charAt(i), afterFile));
+        }
+        return percent;
+    }
+
+    public static String distribution (char letter, String text) {
+
+        int letterCount = 0;
+        int totalCount= 0;
+        double distribution;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+
+        for (int i = 0; i < text.length(); i++) {
+
+            if (text.charAt(i) == letter)
+                letterCount++;
+
+            totalCount++;
         }
 
+        distribution = (double)letterCount / totalCount * 100;
 
+        String newFormat = df.format(distribution);
 
+        String distributionFormat = letter + " = " + newFormat + "%";
 
+        return distributionFormat;
+    }
 
-    public static void main(String[] args)
-    {
+    public static void main (String [] args) {
+
+        ArrayList<String> out = new ArrayList();
+
+        File myFile = new File("/Users/c4q-madelyntavarez/Desktop/accesscode/HW_04-03/src/nyc/c4q/accesscode/Dickens");
+
+        out = calculate(myFile);
+
+        for (String words : out)
+            System.out.println(words);
 
     }
 }
